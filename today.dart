edb7d24903efb5d50922f4b28382fe4851448d55
Akash Ramesh <akashnairr2003@gmail.com>
2024-07-15 08:55:12 +0530

    todays file commited

---
+++ today.dart
@@ -0,0 +1,46 @@
+/*void main(){
+
+ double m,n=365;
+ double sum=0;
+ while( n > 0 )    
+{    
+ m = n%10;    
+ sum =sum+m;    
+ n = n~/10;    
+}    
+ print("$sum");
+}*/
+
+            //reverse
+/*void main(){
+        int n= 365;
+        int rev= 0;
+        while(n!=0) {
+            final rem=n%10;
+            rev = rev*10+n%10;
+            n= n~/10;
+        }
+        print("$rev");
+    }*/
+
+                        //fizzbuzz
+
+/*void main(){
+    for(int i=1;i<100;i++){
+        if(i%3==0 && i%5==0){
+            print("fizzbuzz");
+            }
+            else if(i%3==0){
+                print("fizz");
+                }
+                else if(i%5==0){
+                    print("buzz");
+                    }
+            else{
+            print("$i");
+            }
+    }
+}*/
+    
+                  // odd or eeven
+                  
